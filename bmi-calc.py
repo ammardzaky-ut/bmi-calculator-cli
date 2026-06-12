87c942bc969b63e296ecc59e1c0178a5b303b56c
Ammar Dzaky <mammardzaky0@gmail.com>
2026-06-12 07:05:09 +0700

    first commit

---
+++ bmi-calc.py
@@ -0,0 +1,18 @@
+
+print("BMI Calculator")
+print("=================")
+
+berat = float(input("Enter your body weight (kg): "))
+tinggi = float(input("Enter your body height (m): "))55
+
+bmi = berat / (tinggi * tinggi)
+print("Your BMI is:", bmi)
+if bmi < 18.5:
+    print("Your category is: Underweight")
+elif 18.5 <= bmi < 25:
+    print("Your category is: Normal weight")
+elif 25 <= bmi < 30:
+    print("Your category is: Overweight")
+else:
+    print("Your category is: Obesity")
+# Program ini menghitung BMI berdasarkan berat dan tinggi yang dimasukkan oleh pengguna, kemudian memberikan kategori berdasarkan hasil BMI tersebut.   
